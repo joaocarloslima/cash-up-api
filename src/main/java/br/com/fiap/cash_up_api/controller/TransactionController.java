5b7d29afca174b608f998ec95f33c561a4e400a3
joaocarloslima <joaocarloslima@me.com>
2025-04-10 09:39:57 -0300

    add filter and pagination

--- src/main/java/br/com/fiap/cash_up_api/controller/TransactionController.java
+++ src/main/java/br/com/fiap/cash_up_api/controller/TransactionController.java
@@ -1,15 +1,19 @@
 package br.com.fiap.cash_up_api.controller;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.cash_up_api.model.Transaction;
 import br.com.fiap.cash_up_api.repository.TransactionRepository;
+import br.com.fiap.cash_up_api.specification.TransactionSpecification;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -17,16 +21,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TransactionController {
 
-    record TransactionFilter(String description, LocalDate startDate, LocalDate endDate) {
+    public record TransactionFilter(String description, LocalDate startDate, LocalDate endDate) {
     }
 
     @Autowired
     private TransactionRepository repository;
 
     @GetMapping
-    public List<Transaction> index(TransactionFilter filter) {
-
-        return repository.findAll();
+    public Page<Transaction> index(TransactionFilter filter,
+            @PageableDefault(size = 10, sort = "date", direction = Direction.DESC) Pageable pageable) {
+        var specification = TransactionSpecification.withFilters(filter);
+        return repository.findAll(specification, pageable);
     }
 
 }
