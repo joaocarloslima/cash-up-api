33b032664ae64df18b5dcb524212eda4539a9f15
joaocarloslima <joaocarloslima@me.com>
2025-05-06 12:15:18 -0300

    feat: update CORS configuration and enhance user controller with create user endpoint

--- src/main/java/br/com/fiap/cash_up_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/cash_up_api/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.fiap.cash_up_api.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -27,13 +32,27 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //.httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfig(){
+        var config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000/"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     // @Bean
--- src/main/java/br/com/fiap/cash_up_api/controller/UserController.java
+++ src/main/java/br/com/fiap/cash_up_api/controller/UserController.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.cash_up_api.model.User;
+import br.com.fiap.cash_up_api.model.UserRole;
 import br.com.fiap.cash_up_api.repository.UserRepository;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 
     @Autowired
@@ -21,6 +25,12 @@ public class UserController {
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        user.setRole(UserRole.USER);
+        return repository.save(user);
     }
 
 }
